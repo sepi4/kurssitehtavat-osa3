bbacb16159657813aebb34084695c660eb62cd12
Sergei <sepolish@student.jyu.fi>
2019-09-16 19:22:58 +0300

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send(`<p>moikka</p>
+  <p>json-data on osoitteessa '.../api/persons'</p>`)
+})
+
+let notes = [
+  {
+    name: "kissa",
+    number: "111",
+    id: 1,
+  },
+  {
+    name: "koira",
+    number: "222",
+    id: 2,
+  },
+  {
+    name: "apina",
+    number: "333",
+    id: 3,
+  },
+  {
+    name: "sika",
+    number: "444",
+    id: 4,
+  },
+  {
+    name: "hevonen",
+    number: "555",
+    id: 5,
+  },
+]
+
+
+app.get('/api/persons', (req, res) => {
+  res.json(notes)
+})
+
+const port = 3001
+app.listen(port)
+console.log(`Server running on port ${port}`)
