c6a0c79fe70cbcbaad6b8e7ea929a1bf93c40211
Sergei <sepolish@student.jyu.fi>
2019-09-17 03:16:57 +0300

    trying to build

---
+++ build/precache-manifest.7befcede159af7e345573e7adfb478a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74510fa640d89c3e9db9f5a2ed8aa7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "053c98e1dedaa57e4e3f",
+    "url": "/static/css/main.c2f28f6d.chunk.css"
+  },
+  {
+    "revision": "721a7c77ca95efc9970e",
+    "url": "/static/js/2.02ed846c.chunk.js"
+  },
+  {
+    "revision": "053c98e1dedaa57e4e3f",
+    "url": "/static/js/main.e7746d35.chunk.js"
+  },
+  {
+    "revision": "6b9465e6ebe70eea55d0",
+    "url": "/static/js/runtime~main.a27f5722.js"
+  }
+]);
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.static('build'))
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 
