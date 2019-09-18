9bc2f5284d5cf728ef1f0e4b360ea05900c0cf40
Sergei <sepolish@student.jyu.fi>
2019-09-18 03:28:02 +0300

    3.20*: puhelinluettelo ja tietokanta, step8

--- models/person.js
+++ models/person.js
@@ -14,9 +14,17 @@ mongoose.connect(url, { useNewUrlParser: true })
 
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  number: { type: String, required: true }
-  // id: Number,
+  name: { 
+    type: String, 
+    required: true, 
+    unique: true,
+    minlength: 3,
+  },
+  number: { 
+    type: String, 
+    required: true,
+    minlength: 8,
+  }
 })
 personSchema.plugin(uniqueValidator)
 
